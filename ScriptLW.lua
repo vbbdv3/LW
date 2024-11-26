4492ae276bac0e7ce494608bc86f5db6caf955ba
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-11-27 02:06:30 +0500

    Update ScriptLW.lua

--- ScriptLW.lua
+++ ScriptLW.lua
@@ -132,3 +132,35 @@ wait(300)
 end
 end)
 
+local btns = serv:Channel("Farm")
+
+btns:Button("Bow", function()
+_G.Bowledc_enabled = not _G.Bowledc_enabled
+while _G.Bowledc_enabled do
+local args = {
+    [1] = Vector3.new(-654.9907836914062, 5.291313171386719, 411.4891357421875),
+    [2] = 12
+}
+game:GetService("Players").LocalPlayer.Character.Bow.SwordScript.Shoot:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+
+btns:Button("Bazooka", function()
+_G.Bazooka_enabled = not _G.Bazooka_enabled
+while _G.Bazooka_enabled do
+local args = {
+    [1] = Vector3.new(-837.376220703125, 7.115766525268555, 503.55438232421875)
+}
+game:GetService("Players").LocalPlayer.Character.Bazooka.ServerScript.Throw:FireServer(unpack(args))
+wait(0.5)
+end
+end)
+
+btns:Button("RightSword", function()
+_G.DualSwords_enabled = not _G.DualSwords_enabled
+while _G.DualSwords_enabled do
+game:GetService("Players").LocalPlayer.Character.RightSword.SwordScript.Activate:FireServer()
+wait(0.1)
+end
+end)
